c1ae9f2d9997a97035b025b3457d48e5415a5403
wagnerSfarias <wagner@universalhost.com.br>
2023-08-10 21:35:32 -0300

    create page sign up

---
+++ src/Pages/Register/index.js
@@ -0,0 +1,88 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as Yup from 'yup'
+
+import LoginImg from '../../assets/logo.png'
+import {
+  Container,
+  Form,
+  ContainerItens,
+  LoginImage,
+  Label,
+  Input,
+  Button,
+  SignInlink,
+  ErrorMessage
+} from './styles'
+
+export default function Register() {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é obrigatório.'),
+    email: Yup.string()
+      .email('Digite um e-mail válido.')
+      .required('O e-mail é obrigatório.'),
+    password: Yup.string()
+      .required('A senha é obrigatória.')
+      .min(6, 'A senha dete ter pelo menos 6 digítos.'),
+    confirmPassword: Yup.string()
+      .required('A senha é obrigatória.')
+      .oneOf([Yup.ref('password')], 'As senhas devem ser iguais.')
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({ resolver: yupResolver(schema) })
+
+  const onsubmit = data => console.log(data)
+
+  return (
+    <Container>
+      <Form>
+        <LoginImage src={LoginImg} alt="login-image" />
+        <ContainerItens>
+          <h1>Criar Conta</h1>
+          <form noValidate onSubmit={handleSubmit(onsubmit)}>
+            <Label>Nome</Label>
+            <Input
+              type="text"
+              {...register('name')}
+              error={errors.name?.message}
+            />
+            <ErrorMessage>{errors.name?.message}</ErrorMessage>
+
+            <Label>Email</Label>
+            <Input
+              type="email"
+              {...register('email')}
+              error={errors.email?.message}
+            />
+            <ErrorMessage>{errors.email?.message}</ErrorMessage>
+
+            <Label>Senha</Label>
+            <Input
+              type="password"
+              {...register('password')}
+              error={errors.password?.message}
+            />
+            <ErrorMessage>{errors.password?.message}</ErrorMessage>
+
+            <Label>Confirmar Senha</Label>
+            <Input
+              type="password"
+              {...register('confirmPassword')}
+              error={errors.confirmPassword?.message}
+            />
+            <ErrorMessage>{errors.confirmPassword?.message}</ErrorMessage>
+
+            <Button type="submit">Criar</Button>
+          </form>
+
+          <SignInlink>Já possui conta ? Entrar</SignInlink>
+        </ContainerItens>
+      </Form>
+    </Container>
+  )
+}
---
+++ src/Pages/Register/styles.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #7890f4;
+`
+export const Form = styled.div`
+  display: flex;
+  max-width: 80%;
+  max-height: 90%;
+`
+export const LoginImage = styled.img`
+  width: 60%;
+  background: #fff;
+  border-radius: 10px 0 0 10px;
+`
+export const ContainerItens = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #e8eefe;
+  border-radius: 0 10px 10px 0;
+  width: 40%;
+  padding: 30px 50px;
+  color: #fff;
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  h1 {
+    font-weight: 400;
+    font-size: 28px;
+    color: #7890f4;
+    text-align: center;
+    margin: 10px 0;
+  }
+`
+export const Label = styled.p`
+  font-weight: 500;
+  line-height: 14px;
+  color: #7890f4;
+  margin: 15px 0 5px;
+`
+export const Input = styled.input`
+  height: 38px;
+  box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
+  border-radius: 5px;
+  border: ${props => (props.error ? '2px solid #cc1717' : 'none')};
+  padding-left: 10px;
+  font-size: 15px;
+  color: #313131;
+`
+export const SignInlink = styled.p`
+  font-weight: 300;
+  text-align: end;
+  margin: 20px 0;
+  line-height: 16px;
+  color: #5052ff;
+  font-size: 16px;
+  cursor: pointer;
+`
+export const Button = styled.button`
+  width: 70%;
+  height: 40px;
+  background: #fe7569;
+  border-radius: 20px;
+  border: none;
+  font-weight: 500;
+  text-align: center;
+  cursor: pointer;
+  color: #fff;
+  font-size: 18px;
+  margin: 20px auto;
+
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    opacity: 0.6;
+  }
+`
+export const ErrorMessage = styled.p`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+  color: #cc1717;
+  margin-top: 5px;
+`
