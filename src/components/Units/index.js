d1331a7506d91ada3637a539a8b3747ef50692d0
wagnerSfarias <wagner@universalhost.com.br>
2023-08-21 19:28:39 -0300

    creating list-units

---
+++ src/components/Units/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+
+import api from '../../service/api'
+import { Title } from '../CarouselExperiences/styles'
+import { ContainerUnits, Button } from './styles'
+
+export default function Units() {
+  const [units, setUnits] = useState([])
+
+  useEffect(() => {
+    async function loadUnits() {
+      const { data } = await api.get('/units')
+      setUnits(data)
+    }
+    loadUnits()
+  }, [])
+
+  return (
+    <>
+      <Title>Unidades</Title>
+      <ContainerUnits>
+        {units &&
+          units.map(unit => (
+            <Button
+              key={unit.id}
+              to={{
+                pathname: '/quartos',
+                state: { unitId: unit.id }
+              }}
+            >
+              <img src={unit.url} />
+              <p>{unit.name}</p>
+            </Button>
+          ))}
+      </ContainerUnits>
+    </>
+  )
+}
---
+++ src/components/Units/styles.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const ContainerUnits = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 60px;
+  margin-bottom: 30px;
+  padding: 0 3%;
+`
+export const Button = styled(Link)`
+  max-height: 250px;
+  position: relative;
+  border: none;
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+  img {
+    border-radius: 10px;
+    max-height: 250px;
+    /* height: 100%; */
+    width: 100%;
+  }
+  p {
+    position: absolute;
+    bottom: 0;
+    font-size: 22px;
+    font-weight: 500;
+    background-color: rgba(164, 161, 161, 0.6);
+    color: #fff;
+    width: 100%;
+    text-align: right;
+    padding: 2% 3%;
+    border-radius: 0px 0 10px 10px;
+  }
+`
