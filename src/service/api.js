af19b2955c73bde5c15dbb4dbe860ea08cd5c313
wagnerSfarias <wagner@universalhost.com.br>
2023-08-28 10:39:11 -0300

    interceptor token

--- src/service/api.js
+++ src/service/api.js
@@ -4,4 +4,11 @@ const apiHotel = axios.create({
   baseURL: 'http://localhost:3001'
 })
 
+apiHotel.interceptors.request.use(async config => {
+  const userData = await localStorage.getItem('userData:hotel')
+  const token = userData && JSON.parse(userData).token
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
 export default apiHotel
