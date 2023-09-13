b146aacf830e47638cdfe1c9ac4ee7b739b86a60
wagnerSfarias <wagner@universalhost.com.br>
2023-09-13 19:00:44 -0300

    redirect if not admin

--- src/routes/private-route.js
+++ src/routes/private-route.js
@@ -2,15 +2,21 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
-export default function PrivateRoutes({ component, ...rest }) {
+export default function PrivateRoutes({ component, isAdmin, ...rest }) {
   const user = localStorage.getItem('userData:hotel')
 
   if (!user) {
     return <Redirect to="/login" />
   }
+
+  if (isAdmin && !JSON.parse(user).admin) {
+    return <Redirect to="/" />
+  }
+
   return <Route {...rest} component={component} />
 }
 
 PrivateRoutes.propTypes = {
-  component: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
+  component: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
+  isAdmin: PropTypes.bool
 }
