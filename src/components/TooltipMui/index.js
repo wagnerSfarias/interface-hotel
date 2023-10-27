c8ffcb51122dc903b6856a89ecc59a0f57e57a69
wagnerSfarias <wagner@universalhost.com.br>
2023-10-27 19:44:29 -0300

    first commit

---
+++ src/components/TooltipMui/index.js
@@ -0,0 +1,29 @@
+import { styled } from '@mui/material/styles'
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const LightTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: '#305369'
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    // backgroundColor: '#7890F4',
+    backgroundColor: '#305369',
+    height: '30px',
+    display: 'flex',
+    alignItems: 'center',
+    color: '#FFF',
+    fontSize: 14
+  }
+}))
+
+export function TooltipMui({ children, ...rest }) {
+  return <LightTooltip {...rest}>{children}</LightTooltip>
+}
+
+TooltipMui.propTypes = {
+  children: PropTypes.node
+}
