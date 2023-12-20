985776a44aea64a6d30456855b34a7fe42b2a9e7
wagnerSfarias <wagner@universalhost.com.br>
2023-12-19 23:24:08 -0300

    responsiveness cards units

--- src/components/Units/styles.js
+++ src/components/Units/styles.js
@@ -4,13 +4,20 @@ import styled from 'styled-components'
 export const ContainerUnits = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-top: 60px;
-  margin-bottom: 30px;
+  margin: 4% auto;
   padding: 0 3%;
   gap: 2%;
+  max-width: 1250px;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 6%;
+  }
 `
 export const Button = styled(Link)`
-  max-height: 250px;
+  display: flex;
   position: relative;
   border: none;
   cursor: pointer;
@@ -23,17 +30,26 @@ export const Button = styled(Link)`
     border-radius: 10px;
     max-height: 250px;
     width: 100%;
+    height: 100%;
   }
   p {
     position: absolute;
     bottom: 0;
     font-size: 22px;
     font-weight: 500;
-    background-color: rgba(164, 161, 161, 0.6);
-    color: #fff;
+    background-color: rgba(224, 231, 238, 0.7);
+    color: #305369;
     width: 100%;
     text-align: right;
     padding: 2% 3%;
     border-radius: 0px 0 10px 10px;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin-bottom: 3%;
+
+    &:hover {
+      transform: none;
+    }
   }
 `
