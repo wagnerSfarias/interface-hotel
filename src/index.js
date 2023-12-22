662cb33140ec56102088a57108427420440589b3
wagnerSfarias <wagner@universalhost.com.br>
2023-12-22 11:40:30 -0300

    inserting menuContext

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 
-import { UserProvider } from './hooks/UserContext'
+import AppProvider from './hooks/index'
 import Routes from './routes'
 import GlobalStyles from './styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
-    <UserProvider>
+    <AppProvider>
       <Routes />
-    </UserProvider>
+    </AppProvider>
     <ToastContainer autoClose={4000} />
     <GlobalStyles />
   </>
