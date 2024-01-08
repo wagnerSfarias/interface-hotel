04a43ec4184935e1c08a5f9e12e74e216a26a7f6
wagnerSfarias <wagner@universalhost.com.br>
2024-01-08 18:52:37 -0300

    inserting bedroom page responsive

--- src/Pages/Bedrooms/styles.js
+++ src/Pages/Bedrooms/styles.js
@@ -8,6 +8,7 @@ const rotate = keyframes`
 export const Container = styled.div`
   min-height: 100vh;
   background-color: #7890f4;
+  padding-bottom: 1%;
 `
 export const ContainerBedrooms = styled.div`
   display: flex;
@@ -15,6 +16,10 @@ export const ContainerBedrooms = styled.div`
   align-items: center;
   width: 60%;
   margin: 0 auto;
+
+  @media screen and (max-width: 480px) {
+    width: 80%;
+  }
 `
 export const CardBedrooms = styled.div`
   display: flex;
@@ -23,8 +28,13 @@ export const CardBedrooms = styled.div`
   flex-direction: column;
   margin: 2% 0;
   border-radius: 5px;
-`
+  position: relative;
+  overflow: hidden;
 
+  @media screen and (max-width: 768px) {
+    padding-bottom: 20%;
+  }
+`
 export const Descriptions = styled.div`
   width: 100%;
   display: flex;
@@ -34,19 +44,27 @@ export const Descriptions = styled.div`
     max-height: 270px;
     width: 40%;
   }
-`
 
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+
+    img {
+      width: 100%;
+      max-height: 230px;
+    }
+  }
+`
 export const Detail = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   width: 100%;
   padding: 15px;
-  justify-content: space-between;
 
   h1 {
     color: #fe7569;
     font-weight: 400;
-    font-size: 28px;
+    font-size: 24px;
   }
   p {
     color: #305369;
@@ -55,11 +73,21 @@ export const Detail = styled.div`
     color: #fabc05;
   }
 
+  @media screen and (max-width: 768px) {
+    padding: 2%;
+
+    h1 {
+      font-size: 20px;
+      font-weight: 500;
+    }
+  }
+
   button {
     border: none;
     background-color: transparent;
     color: #fe7569;
     font-size: 20px;
+    font-weight: 500;
     cursor: pointer;
     position: relative;
     width: 200px;
@@ -89,6 +117,22 @@ export const Detail = styled.div`
       display: flex;
       align-items: center;
       justify-content: center;
+    }
+
+    @media screen and (max-width: 768px) {
+      position: absolute;
+      bottom: 2%;
+      right: 0;
+      left: 0;
+      width: 150px;
+      height: 40px;
+      font-size: 18px;
+      font-weight: 500;
+      border: 2px solid #fe7569;
+
+      &:hover:before {
+        display: none;
+      }
     }
   }
 `
--- src/components/ListAmenities/styles.js
+++ src/components/ListAmenities/styles.js
@@ -5,22 +5,36 @@ export const Container = styled.div`
   justify-content: space-around;
   margin-bottom: 2%;
   width: 100%;
+  gap: 1%;
 
   div {
     padding: 15px 10px;
     border-radius: 5px;
     display: flex;
-    gap: 10px;
+    gap: 5px;
     box-shadow: 5px 4px 10px 2px rgba(0, 0, 0, 0.3);
   }
 
   p {
     color: #305369;
     font-size: 14px;
+    text-align: center;
   }
 
   .icon {
     color: #fe7569;
     font-size: 18px;
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    width: 90%;
+    margin: 0 auto;
+
+    div {
+      margin-bottom: 5%;
+      gap: 2%;
+      box-shadow: 4px 2px 10px 2px rgba(0, 0, 0, 0.3);
+    }
   }
 `
