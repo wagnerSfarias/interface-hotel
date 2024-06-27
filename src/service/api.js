ddd56fd3ac64dcea16f71d9c6bb20ec819615243
wagnerSfarias <wagner@universalhost.com.br>
2024-06-26 23:27:55 -0300

    feat: inserting response interceptors

--- src/service/api.js
+++ src/service/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const apiHotel = axios.create({
   baseURL: 'http://localhost:3001'
@@ -11,4 +12,23 @@ apiHotel.interceptors.request.use(async config => {
   return config
 })
 
+apiHotel.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error.response?.status === 401) {
+      toast.error('Ocorreu um erro com sua autenticação! Tente novamente.')
+
+      localStorage.removeItem('userData:hotel')
+
+      setTimeout(() => {
+        window.location.href = '/login'
+      }, 2000)
+    } else {
+      toast.error('Falha no sistema! Tente novamente.')
+    }
+    return Promise.reject(error)
+  }
+)
 export default apiHotel
